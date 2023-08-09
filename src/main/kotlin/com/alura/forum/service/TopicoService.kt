ac00cc8c1138e9996fd90ed9b172e3523f7af0e9
aislanlima13 <aislan.martins99@gmail.com>
2023-08-09 13:43:48 -0300

    Cria funções para deletar e atualizar tópicos

--- src/main/kotlin/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/com/alura/forum/service/TopicoService.kt
@@ -1,5 +1,6 @@
 package com.alura.forum.service
 
+import com.alura.forum.dto.AtualizacaoTopicoForm
 import com.alura.forum.dto.NovoTopicoForm
 import com.alura.forum.dto.TopicoView
 import com.alura.forum.mapper.TopicoFormMapper
@@ -31,5 +32,30 @@ class TopicoService(
         val topico = topicoFormMapper.map(dto)
         topico.id = topicos.size.toLong() + 1
         topicos = topicos.plus(topico)
+    }
+
+    fun atualizar(form: AtualizacaoTopicoForm) {
+        val topico = topicos.stream().filter { t ->
+            t.id == form.id
+        }.findFirst().get()
+
+        topicos = topicos.minus(topico).plus(Topico(
+                id = form.id,
+                titulo = form.titulo,
+                mensagem = form.mensagem,
+                curso = topico.curso,
+                autor = topico.autor,
+                respostas = topico.respostas,
+                status = topico.status,
+                dataCriacao = topico.dataCriacao
+        ))
+    }
+
+    fun deletar(id: Long) {
+        val topico = topicos.stream().filter { t ->
+            t.id == id
+        }.findFirst().get()
+
+        topicos = topicos.minus(topico)
     }
 }
