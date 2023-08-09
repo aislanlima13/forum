df33222f78310b198de8027b21edea3407803f25
aislanlima13 <aislan.martins99@gmail.com>
2023-08-09 14:01:59 -0300

    Adiciona melhorias para boas práticas de REST

--- src/main/kotlin/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/com/alura/forum/service/TopicoService.kt
@@ -28,18 +28,20 @@ class TopicoService(
         return topicoViewMapper.map(topico)
     }
 
-    fun cadastrar(dto: NovoTopicoForm) {
+    fun cadastrar(dto: NovoTopicoForm): TopicoView{
         val topico = topicoFormMapper.map(dto)
         topico.id = topicos.size.toLong() + 1
         topicos = topicos.plus(topico)
+
+        return topicoViewMapper.map(topico)
     }
 
-    fun atualizar(form: AtualizacaoTopicoForm) {
+    fun atualizar(form: AtualizacaoTopicoForm): TopicoView {
         val topico = topicos.stream().filter { t ->
             t.id == form.id
         }.findFirst().get()
 
-        topicos = topicos.minus(topico).plus(Topico(
+        val topicoAtualizado = Topico(
                 id = form.id,
                 titulo = form.titulo,
                 mensagem = form.mensagem,
@@ -48,7 +50,11 @@ class TopicoService(
                 respostas = topico.respostas,
                 status = topico.status,
                 dataCriacao = topico.dataCriacao
-        ))
+        )
+
+        topicos = topicos.minus(topico).plus(topicoAtualizado)
+
+        return topicoViewMapper.map(topicoAtualizado)
     }
 
     fun deletar(id: Long) {
